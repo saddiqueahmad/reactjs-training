786db4d18a8def65e1e1815739ef689dd3b81a45
saddiqueahmad <106383465+saddiqueahmad@users.noreply.github.com>
2022-08-13 23:17:39 +0500

    Create AdvMethodsExercise.js

---
+++ css selectors/javascriptproject/Advanced methods/AdvMethodsExercise.js
@@ -0,0 +1,44 @@
+//#3 create two classes: an Animal class and a Mamal class. 
+// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+
+class Animal {
+  constructor(name, type, color) {
+    this.name = name;
+    this.type = type;
+    this.color = color;
+  }
+
+  introduce() {
+     
+     console.log(`Hi I am ${this.name}, I'm a ${this.type}, of color ${this.color}`);
+}
+
+}
+
+class Mamal extends Animal {
+  constructor(name, type, color) {
+    super(name, type, color);
+  }
+
+  sound() {
+  	
+  		console.log(`Moooooooooo I'm ${this.name}, a ${this.type}, of color ${this.color}`);
+  	} 
+}
+
+const mamal = new Mamal("Cow","healthy","black");
+
+mamal.introduce();
+mamal.sound();
+
+//Evaluate these:
+//#1
+[2] === [2] => false
+{} === {} => false
+
+//#2 what is the value of property a for each object.
+const object1 = { a: 5 }; 
+const object2 = object1; = true
+const object3 = object2; = true
+const object4 = { a: 5}; 
+object1.a = 4;
